38d0749d81c340b0bba0f1d9bf1d084a004c9114
Nkenge Simms <simmsn@yahoo.com>
2020-08-29 13:06:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,9 +23,9 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find{|cheddar|cheese_types== "cheddar", "gouda","camembert"}
+  array.find{|cheddar|cheese_types== "cheddar", "gouda","camembert" }
     
-    return array
+   
     
     
  
