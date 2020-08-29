db361837baa0cb6380abef76a19c860f0619eee4
Nkenge Simms <simmsn@yahoo.com>
2020-08-29 13:09:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(array)
   array.find{|gouda|cheese_types== "gouda"}
   array.find{|camembert|cheese_types== "camembert"}
    end 
-  return array
+  return array.find
   
     
  
