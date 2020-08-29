8ea050a753b21fdebc067bd21b1a4eef466ee63c
Nkenge Simms <simmsn@yahoo.com>
2020-08-29 13:33:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,8 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.find {|cheddar|cheese_types == "cheddar"}
- return true if cheddar.include?(str)
+  array.find {|cheddar.include?( "cheddar")}
+ #return true if cheddar.include?(str)
    
 
   #array.find{|gouda|cheese_types== "gouda"}
